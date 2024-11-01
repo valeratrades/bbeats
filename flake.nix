0207a6bb78fa3af28b138297830c4fc3e1163eff
valeratrades <v79166789533@gmail.com>
2024-11-01 13:24:37 +0000

    _

--- flake.nix
+++ flake.nix
@@ -18,11 +18,10 @@
           version = manifest.version;
 
           buildInputs = with pkgs; [
-            openssl
-            openssl.dev
+						ocamlPackages.alsa
           ];
           nativeBuildInputs = with pkgs; [ pkg-config ];
-          env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          #env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
           cargoLock.lockFile = ./Cargo.lock;
           src = pkgs.lib.cleanSource ./.;
